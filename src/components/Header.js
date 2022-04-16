653559c349ea3f07956e756f66869cf6f3fd4de4
Code Malayalam <ktucsnotes@gmail.com>
2022-04-16 13:46:43 +0000

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { clearUserInfo, isLoggedIn } from "../utils/Common";
+import { clearUserInfo } from "../utils/Common";
 import "./Header.css";
 import LogedInOutlet from "./outlets/LogedInOutlet";
 import PrivateLink from "./links/PrivateLink";
 
 export default function Header() {
-    const navigate = useNavigate();
 
     return (
         <div>
@@ -23,13 +21,9 @@ export default function Header() {
                     <PrivateLink to="usage">
                         <span> Usage </span>
                     </PrivateLink>
-                    <span style={{flexGrow: 1}}/>
-                    {
-                        isLoggedIn() && <span onClick={() => {
-                            clearUserInfo();
-                            navigate('login');
-                        }}> Logout </span>
-                    }
+                    <PrivateLink to="login" className="logout" onClick={() => clearUserInfo()}>
+                        <span> Logout </span>
+                    </PrivateLink>
             </div>
             <div>
                 <LogedInOutlet />
@@ -37,3 +31,4 @@ export default function Header() {
         </div>
     );
 }
+
